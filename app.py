5ae7ea89586aa720c73d82f91e9c7ee3934a8767
melih <dev.mberatsanli@gmail.com>
2025-01-19 18:16:24 +0100

    docker

--- app.py
+++ app.py
@@ -114,4 +114,4 @@ def get_project(id):
 if __name__ == '__main__':
     if not JWT_SECRET:
         raise ValueError("JWT_SECRET environment variable is required")
-    app.run(debug=True, host='0.0.0.0', port=8005)
+    app.run(debug=True, host='0.0.0.0', port=os.getenv('PORT', 8000))
